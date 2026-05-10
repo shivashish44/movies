77c757cb4535098985b21e25a75fe2cf192cffa5
SHIV <shivashish4444@gmail.com>
2026-05-10 17:15:30 +0530

    Update redeem buttons with new URLs

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,8 +44,8 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot")],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")]
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/MOVIE_BOX_ROBOT")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/ll_DEVIL_SHIV_ll")]
             ]
         )
 
