316a4299147d18ccdaa20f633d901eb738adae3b
SHIV <shivashish4444@gmail.com>
2026-05-11 20:26:41 +0530

    Update Channel.py

--- plugins/helper/Channel.py
+++ plugins/helper/Channel.py
@@ -3,13 +3,13 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 
 # Replace this with your own channel ID
-CHANNEL_ID = -1003738160966
+CHANNEL_ID = -1003910381589
 
 @Client.on_message(filters.channel & filters.media)
 async def add_button(client, message):
     if message.chat.id == CHANNEL_ID:
         button = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/+WxW5DBNJBkU5ODY1")]]
+            [[InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/+sLEVq3pYIiBiNGI9")]]
         )
         
         try:
